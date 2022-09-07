9b48d70f010fdaf7537b741787cc02f99e81b764
MecMelt <eltonlimeira10@gmail.com>
2022-09-06 21:27:43 -0300

    Exerciciocondicionais

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,5 @@
-const num = //seu numero aqui
+let numero = prompt("Digite um número e verifique se é par!")
 
-if(){
-    console.log("sua mensagem aqui");
+if(numero % 2 == 0){
+    console.log("o número é par!")
 }
